92e1a0e0f4531a48bbe3a4cd7e36026b6ebc0b92
Buglloc <buglloc@yandex.ru>
2017-02-28 20:42:03 +0300

    Added some utils

--- httpfeature/utils.go
+++ httpfeature/utils.go
@@ -7,6 +7,7 @@ import (
 	"math/rand"
 	"strconv"
 	"sort"
+	"math"
 )
 
 const concurrency = 30
@@ -14,13 +15,12 @@ var NotAlphaNumSyms []rune
 var AlphaNumSyms []rune
 var HttpMethods = []string{
 	// RFC 2616
-	"GET",	"HEAD",	"POST",	"PUT",	"DELETE", "OPTIONS", "TRACE", "CONNECT",
+	"GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS", "TRACE", "CONNECT",
 	// RFC 2518
 	"PROPFIND", "PROPPATCH", "MKCOL", "COPY", "MOVE", "LOCK", "UNLOCK",
 	// RFC 3744
 	"ACL",
 }
-
 
 func init() {
 	for i := 0; i < 0xFF; i++ {
@@ -122,4 +122,81 @@ func PrintableBool(data bool) string {
 		return "Yes"
 	}
 	return "No"
+}
+
+func PrintableBytes(bytes int) string {
+	const (
+		BYTE     = 1.0
+		KILOBYTE = 1024 * BYTE
+		MEGABYTE = 1024 * KILOBYTE
+		GIGABYTE = 1024 * MEGABYTE
+		TERABYTE = 1024 * GIGABYTE
+	)
+
+	unit := ""
+	value := float32(bytes)
+
+	switch {
+	case bytes >= TERABYTE:
+		unit = "T"
+		value = value / TERABYTE
+	case bytes >= GIGABYTE:
+		unit = "G"
+		value = value / GIGABYTE
+	case bytes >= MEGABYTE:
+		unit = "M"
+		value = value / MEGABYTE
+	case bytes >= KILOBYTE:
+		unit = "K"
+		value = value / KILOBYTE
+	case bytes >= BYTE:
+		unit = "B"
+	case bytes == 0:
+		return "0"
+	}
+
+	stringValue := fmt.Sprintf("%.1f", value)
+	return fmt.Sprintf("%s%siB", stringValue, unit)
+}
+
+func GoldenSectionSearch(beforeX, afterX, prec float64, testFunc func(x float64) float64) float64 {
+	spacing := (3.0 - math.Sqrt(5)) / 2.0
+
+	var x [4]float64
+	var y [4]float64
+
+	x[0], x[3] = beforeX, afterX
+	gapSize := afterX - beforeX
+	x[1], x[2] = x[0]+spacing*gapSize, x[3]-spacing*gapSize
+
+	for i := 0; i < 4; i++ {
+		y[i] = testFunc(x[i])
+	}
+
+	stepCount := int(math.Ceil(math.Log(prec/gapSize) / math.Log(1-spacing)))
+	for i := 0; i < stepCount; i++ {
+		// If we have narrowed down the region to machine
+		// precision, we cannot go any further.
+		if x[1] <= x[0] || x[2] <= x[1] || x[3] <= x[2] {
+			break
+		}
+
+		if y[1] < y[2] {
+			x[3] = x[2]
+			diff := x[3] - x[0]
+			x[1] = x[0] + diff*spacing
+			x[2] = x[3] - diff*spacing
+			y[2], y[3] = y[1], y[2]
+			y[1] = testFunc(x[1])
+		} else {
+			x[0] = x[1]
+			diff := x[3] - x[0]
+			x[1] = x[0] + diff*spacing
+			x[2] = x[3] - diff*spacing
+			y[0], y[1] = y[1], y[2]
+			y[2] = testFunc(x[2])
+		}
+	}
+
+	return (x[3] + x[0]) / 2
 }
