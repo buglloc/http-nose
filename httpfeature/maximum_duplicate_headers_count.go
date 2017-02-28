4af72b5cd5537c05a694481b5ee45f0d91f5649c
Buglloc <buglloc@yandex.ru>
2017-02-28 20:40:32 +0300

    Detect allowed duplicate headers count

---
+++ httpfeature/maximum_duplicate_headers_count.go
@@ -0,0 +1,72 @@
+package httpfeature
+
+import (
+	"fmt"
+)
+
+type MaximumDuplicateHeadersCount struct {
+	BaseFeature
+	Count int
+}
+
+func (f *MaximumDuplicateHeadersCount) Name() string {
+	return "Maximum duplicate headers count"
+}
+
+func (f *MaximumDuplicateHeadersCount) ToString() string {
+	if f.Count == 0 {
+		return "n/a"
+	}
+	return fmt.Sprintf("%d", f.Count)
+}
+
+func (f *MaximumDuplicateHeadersCount) Collect() error {
+	if f.Features.GetDuplicateHeaders().Action >= DUPLICATE_HEADERS_ALLOWED {
+		max := f.Features.GetMaximumHeadersCount().Count + 1
+		if max == 0 {
+			max = MAX_HEADERS_COUNT
+		}
+		f.Count = f.check(max)
+	} else {
+		f.Count = 0
+	}
+
+	return nil
+}
+
+func (f *MaximumDuplicateHeadersCount) check(maxCount int) int {
+	counts := make([]int, maxCount)
+	sem := make(chan bool, concurrency)
+	stop := false
+	for i := 0; i < maxCount && !stop; i++ {
+		sem <- true
+		go func(cur int) {
+			defer func() { <-sem }()
+
+			req := f.BaseRequest.Clone()
+			for k := 0; k < cur; k++ {
+				req.AddHeader("X-Foo", "a")
+			}
+
+			resp, err := f.Client.MakeRequest(req)
+			if err != nil || resp.Status != 200 {
+				counts[cur] = 0
+				stop = true
+			} else {
+				counts[cur] = cur
+			}
+		}(i)
+	}
+
+	for i := 0; i < cap(sem); i++ {
+		sem <- true
+	}
+
+	max := 0
+	for _, v := range counts {
+		if v > max {
+			max = v
+		}
+	}
+	return max
+}
